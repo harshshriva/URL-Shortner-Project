ff8bacba48b8a993a27e83ef179b85a19ca46de7
harsh -uranium <harshshrivastav139@gmail.com>
2022-05-19 21:36:19 +0530

    project4

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,5 +1,5 @@
 const validUrl = require('valid-url')
-
+const randomString = require('randomstring')
 const urlModel = require("../model/urlModel")
 
 
@@ -11,9 +11,9 @@ const redis = require('redis')
 
 const { promisify } = require('util')
 
-const redisClient = redis.createClient(18891, "redis-18891.c91.us-east-1-3.ec2.cloud.redislabs.com", { no_ready_check: true });
+const redisClient = redis.createClient(13142, "redis-13142.c264.ap-south-1-1.ec2.cloud.redislabs.com", { no_ready_check: true });
 
-redisClient.auth("F1uWlcUyoLvjTPzfgZSSzyzrPQaY14md", function(err) {
+redisClient.auth("F22C7UaLHXolOQqet2gUka5oRy9Aj3L3", function(err) {
     if (err) throw err;
 });
 
@@ -24,7 +24,7 @@ redisClient.on("connect", async function() {
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
-// redis password= F22C7UaLHXolOQqet2gUka5oRy9Aj3L3    > that is my redis databse password
+// redis password= F22C7UaLHXolOQqet2gUka5oRy9Aj3L3    > that is my redis atabse password
 // redis enpoint = redis-13142.c264.ap-south-1-1.ec2.cloud.redislabs.com:13142 > that is my port number and coneection string 
 
 // =======================================================================================================================================>
@@ -45,7 +45,50 @@ const isValidRequestBody = function(requestBody) {
 
 // ========================================================================================================================================>
 
+//  < that is first api to short a long link and create a link dta in databse >
 
+const genrateShortUrl = async function(req, res) {
+
+    try {
+
+        let requestBody = req.body
+
+        if (!isValidRequestBody(requestBody)) {
+            return res.status(400).send({ status: false, msg: "body cant be empty please provide details" })
+        }
+
+        if (!isValid(requestBody.longUrl)) {
+            return res.status(400).send({ status: false, msg: "please provide long url" })
+        }
+
+        if (!(validUrl.isWebUri(requestBody.longUrl.trim()))) {
+            return res.status(400).send({ status: false, msg: "provide valid url" })
+        }
+
+
+        let checkUrl = await urlModel.findOne({ longUrl: requestBody.longUrl })
+
+        if (checkUrl) {
+            return res.status(200).send({ status: true, msg: "This url already have shortUrl", data: checkUrl })
+        }
+
+        let urlCode = randomString.generate({ length: 5, charset: 'alphabetic' }).toLowerCase()
+
+        let shortUrl = `http://localhost:3000/${urlCode}`
+
+        requestBody.urlCode = urlCode
+        requestBody.shortUrl = shortUrl
+
+        let createUrl = await urlModel.create(requestBody)
+
+        await SET_ASYNC(`${urlCode}`, JSON.stringify(createUrl))
+
+        res.status(201).send({ status: true, data: createUrl })
+
+    } catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
+    }
+}
 
 // ================================================================================================================================================================================================>
 
@@ -55,18 +98,38 @@ const getUrl = async function(req, res) {
 
     try {
 
-        const urlCode = req.params.urlCode
-        let cachcedData = await GET_ASYNC(`${urlCode}`)
-        if (cachcedUrlData) {
-            return res.status(202).redirect(JSON.Parse(cachcedData))
+        let urlCode = req.params.urlCode.toLowerCase().trim()
+
+        let cahcedUrlData = await GET_ASYNC(`${urlCode}`)
+
+        let abc = JSON.parse(cahcedUrlData)
+
+        if (!cahcedUrlData) {
+            return res.status(400).send({ status: false, msg: "this short url does not exist please provide valid url code " })
         }
-        let getUrl = await urlModel.findOne({ urlCode })
-        if (!getUrl) {
-            return res.status(404).send({ status: false, msg: "url not found" })
+
+        if (cahcedUrlData) {
+
+            console.log("hyy i am if part of get api")
+
+            res.status(301).redirect(301, `${abc.longUrl}`);
+
+
+        } else {
+
+            let urlData = await urlModel.findOne({ urlCode: urlCode })
+
+            if (!urlData) {
+                return res.status(400).send({ status: false, msg: "this short url does not exist please provide valid url code " })
+            }
+
+            await SET_ASYNC(`${urlCode}`, JSON.stringify(urlData))
+
+            console.log("i am else part of get api")
+
+            res.status(301).redirect(301, `${urlData.longUrl}`);
+
         }
-        let url = getUrl.longUrl.longUrl
-        await SET_ASYNC(`${urlCode}`, JSON.parse.stringlfy(url))
-        return res.redirect(url)
     } catch (error) {
         res.status(500).send({ status: false, msg: error.message })
     }
@@ -77,5 +140,5 @@ const getUrl = async function(req, res) {
 // ==================================================================================================================================================================================>
 
 
-// module.exports.genrateShortUrl = genrateShortUrl;
+module.exports.genrateShortUrl = genrateShortUrl;
 module.exports.getUrl = getUrl;
--- src/index.js
+++ src/index.js
@@ -21,6 +21,6 @@ app.use('/', route);
 
 
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
